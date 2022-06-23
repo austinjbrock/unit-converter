1cf99db2ec958fd1cace6c4336445ffa4496a67a
Austin Brock <austinjbrock@gmail.com>
2022-06-22 22:02:58 -0700

    fixed unit conversion

--- app.js
+++ app.js
@@ -14,28 +14,28 @@ let volumeDetails = document.getElementById('volumeDetails')
 let weightDetails = document.getElementById('weightDetails')
 
 // Length Formula
-let meterToFoot = userInput * 3.28084.toFixed(4);
-let footToMeter = userInput * 0.3048.toFixed(4);
+let meterToFoot = userInput * 3.28084.toFixed(2);
+let footToMeter = userInput * 0.3048.toFixed(2);
 
 // Length conversion Details
-footDetails.textContent = `${userInput} meters = ${meterToFoot} Feet | ${userInput} Feet = ${footToMeter} Meters`
+footDetails.textContent = `${userInput} meters = ${meterToFoot} Feet OR ${userInput} Feet = ${footToMeter} Meters`
 
 
 
 // Volume Formula
 let volumeConversion = document.getElementById('volumeConversion')
 
-let literToGallon = userInput * 0.264172.toFixed(4)
-let gallonToLiter = userInput * 3.7854.toFixed(4);
+let literToGallon = userInput * 0.264
+let gallonToLiter = userInput * 3.7854.toFixed(2);
 
-volumeDetails.textContent = `${userInput} Liters = ${literToGallon} Gallons | ${userInput} Gallons = ${gallonToLiter} Liters`
+volumeDetails.textContent = `${userInput} Liters = ${literToGallon} Gallons OR ${userInput} Gallons = ${gallonToLiter} Liters`
 
 
 
 //Mass Formulax
-let kilosToPounds = userInput * 2.20462
-let poundsToKilos = userInput * 0.453592.toFixed(4)
+let kilosToPounds = userInput * 2.20.toFixed(2)
+let poundsToKilos = userInput * 0.453592.toFixed(2)
 
 
 
-weightDetails.textContent = `${userInput} Kilos = ${kilosToPounds} Pounds | ${userInput} Pounds = ${poundsToKilos} Kilos`
+weightDetails.textContent = `${userInput} Kilos = ${kilosToPounds} Pounds OR ${userInput} Pounds = ${poundsToKilos} Kilos`
