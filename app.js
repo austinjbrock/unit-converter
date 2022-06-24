63fbeb2261dc3c8837944f6e1f0ee4d508dec25b
Austin Brock <austinjbrock@gmail.com>
2022-06-23 21:35:26 -0700

    fixed rounding for number conversion on Kilo operation

--- app.js
+++ app.js
@@ -4,18 +4,18 @@
 //update details as soon as units are received
 
 const userInput = prompt('Enter Number')
-let largeUnitDisplay = document.getElementById('inputUnitDisplay')
+const largeUnitDisplay = document.getElementById('inputUnitDisplay')
 
 largeUnitDisplay.innerText = userInput;
 
 //display Unit details
-let footDetails = document.getElementById('footDetails');
-let volumeDetails = document.getElementById('volumeDetails')
-let weightDetails = document.getElementById('weightDetails')
+const footDetails = document.getElementById('footDetails');
+const volumeDetails = document.getElementById('volumeDetails')
+const weightDetails = document.getElementById('weightDetails')
 
 // Length Formula
-let meterToFoot = userInput * 3.28084.toFixed(2);
-let footToMeter = userInput * 0.3048.toFixed(2);
+const meterToFoot = userInput * 3.28084.toFixed(2);
+const footToMeter = userInput * 0.3048.toFixed(2);
 
 // Length conversion Details
 footDetails.textContent = `${userInput} meters = ${meterToFoot} Feet OR ${userInput} Feet = ${footToMeter} Meters`
@@ -23,18 +23,18 @@ footDetails.textContent = `${userInput} meters = ${meterToFoot} Feet OR ${userIn
 
 
 // Volume Formula
-let volumeConversion = document.getElementById('volumeConversion')
+const volumeConversion = document.getElementById('volumeConversion')
 
-let literToGallon = userInput * 0.264
-let gallonToLiter = userInput * 3.7854.toFixed(2);
+const literToGallon = userInput * 0.264
+const gallonToLiter = userInput * 3.7854.toFixed(2);
 
 volumeDetails.textContent = `${userInput} Liters = ${literToGallon} Gallons OR ${userInput} Gallons = ${gallonToLiter} Liters`
 
 
 
 //Mass Formulax
-let kilosToPounds = userInput * 2.20.toFixed(2)
-let poundsToKilos = userInput * 0.453592.toFixed(2)
+const kilosToPounds = userInput * Math.round(2.20)
+const poundsToKilos = userInput * 0.453592.toFixed(2)
 
 
 
